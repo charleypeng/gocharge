fd1b372ba1d7fce650ca479e7a6002d59e9359b5
charleypeng <charleypeng@gmail.com>
2024-07-17 19:48:58 +0800

    feat: add EnvNewLine

--- tools.go
+++ tools.go
@@ -1,5 +1,19 @@
 package gocharge
 
+import (
+	"runtime"
+)
+
 func LogHelper() {
 	panic("to be implemented")
+}
+
+// returns a new line by detecting the os platform
+func EnvNewLine() string {
+	p := runtime.GOOS
+	if p == "windows" {
+		return "\\nn"
+	} else {
+		return "\n"
+	}
 }
