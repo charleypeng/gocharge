15d05d6eb76357df01aecc15f71b7a197d20642e
charleypeng <charleypeng@gmail.com>
2024-08-15 18:59:10 +0800

    feat(module: List): add WhereT func and ToArray

--- list.go
+++ list.go
@@ -5,6 +5,7 @@ type List[T any] struct {
 	items []T
 }
 
+// create a new list struct
 func NewList[T any](data ...[]T) *List[T] {
 	var gclist = List[T]{}
 	if len(data) == 1 {
@@ -14,24 +15,29 @@ func NewList[T any](data ...[]T) *List[T] {
 	return &gclist
 }
 
+// add new items
 func (lst *List[T]) AddItems(data []T) {
 	lst.items = append(lst.items, data...)
 }
 
+// add new item
 func (lst *List[T]) AddItem(data T) {
 	lst.items = append(lst.items, data)
 }
 
+// returns bool if the given element contains the given data
 func (lst *List[T]) Contains(data T) (bool, error) {
 	//todo
 	return true, nil
 }
 
+// sum all the data if it is comparable
 func (lst *List[T]) Sum() any {
 	//todo
 	return nil
 }
 
+// a linq function which returns a predicated list
 func (lst *List[T]) Where(predicate func(T) bool) List[T] {
 
 	var result = *NewList[T]()
@@ -44,6 +50,12 @@ func (lst *List[T]) Where(predicate func(T) bool) List[T] {
 	return result
 }
 
+// get the array list
+func (lst *List[T]) ToArray() []T {
+	return lst.items
+}
+
+// a linq function which returns a predicated list
 func WhereT[T any](lst *[]T, predicate func(T) bool) []T {
 
 	var result = make([]T, 0)
